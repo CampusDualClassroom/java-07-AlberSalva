abdb9a880220c951ef2381bec25152719b8f77db
Alberto Salvado Fernández <95777814+AlberSalva@users.noreply.github.com>
2024-09-27 13:05:42 +0200

    Implemented positionInAList(), sumFirstNaturalNumbers(), showFirstNaturalNumbers(), called them in main()

--- src/main/java/com/campusdual/classroom/Exercise07.java
+++ src/main/java/com/campusdual/classroom/Exercise07.java
@@ -1,32 +1,44 @@
 package com.campusdual.classroom;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise07 {
 
     public static void main(String[] args) {
-
+        positionInAList(15);
+        positionInAList(9);
+        sumFirstNaturalNumbers(15);
+        showFirstNaturalNumbers(5);
     }
 
-    //TODO ↓
-    // ESTE MÉTODO NO ES NECESARIO HACERLO POR PARTE DE LOS ALUMNOS
-    // Crear una List<Integer> con los números del 10 al 1. Luego buscar el elemento
-    // que se le pasa por parámetro.
-    // Si lo encuentra, que imprima el siguiente mensaje → El elemento X se encuentra en la posición: Y
-    // Si NO lo encuentra, que imprima el siguiente mensaje → El elemento X no se encuentra en la lista.
     public static void positionInAList(int num) {
+        List<Integer> numberList = new ArrayList<>();
+        for(int i = 0; i <= 10; i++){
+            numberList.add(i);
+        }
+        int index = numberList.indexOf(num);
+        if (index < 0){
+            System.out.println(num + " no se encuentra en la lista.");
+        } else {
+            System.out.println(num + " se encuentra en la posición: " + index);
+        }
 
     }
 
-    //TODO ↓
-    // Que imprima como mensaje la suma de los primeros N números positivos
     public static void sumFirstNaturalNumbers(int num) {
-
+        int value = 0;
+        for(int i = 1; i <= num; i++){
+            value += i;
+        }
+        System.out.println("La suma es: " + value);
     }
 
-    //TODO ↓
-    // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
-
+        for(int i = 0; i <= num; i++){
+            System.out.println(i);
+        }
     }
 
 }
